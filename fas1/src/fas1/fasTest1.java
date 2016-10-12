00addfc87c5a665290f651d9688242c1e2f976fd
shane <shanecunningham@live.ie>
2016-10-12 10:31:41 +0100

    add test course file

--- fas1/src/fas1/fasTest1.java
+++ fas1/src/fas1/fasTest1.java
@@ -4,7 +4,41 @@ public class fasTest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello shane");
+
+		String lastName = "Cunningham"; // this is a comment
+		int age = 39; // this is a comment
+		String loc = "Newcastle"; // this is a comment
+		String city = ", Galway"; // this is a comment
+		String country = ", Ireland"; // this is a comment
+		String website = "brtd.net"; // this is a comment
+		int num1, num2, num3, num4;
+
+		num1 = 5;
+		num2 = 10;
+		num3 = 2;
+		num4 = 4;
+
+		System.out.println("Hello shane " + lastName + "\nHis age is: " + age); // this
+																				// is
+																				// a
+																				// comment
+		System.out.println("He lives in " + loc + city + country); // this is a
+																	// comment
+		System.out.println("His favourite website is: " + website); // this is a
+																	// comment
+		System.out.println("Computing these number: " + (num1 * num2) + "\nSecond number: " + (num3 + num4));
+		System.out.println("Second test comp: " + (num2 / num3));
+		System.out.println("the num1 before been overwritten: " + num1);
+		num1 = 12;
+		System.out.println("the new number is : " + num1);
+		int sum = num1 + num2;
+		System.out.println("test out: " + sum);
+		
+		
+		
 	}
+	
+	
+	 
 
 }
