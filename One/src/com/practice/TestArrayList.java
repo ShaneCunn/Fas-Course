f4228b1e150639d74d2837c629ce87949fe2b738
shane <shanecunningham@live.ie>
2016-10-18 09:50:52 +0100

    added enchanced for loop  to iterate  arraylists

--- One/src/com/practice/TestArrayList.java
+++ One/src/com/practice/TestArrayList.java
@@ -13,18 +13,40 @@ public class TestArrayList {
 		ArrayList<String> listTest = new ArrayList<String>();
 		
 		
-		listTest.add("test");
-		listTest.add("test2");
+		listTest.add(" green bottles of beer on the wall");
+		listTest.add(" and if ");
+		listTest.add(" green bottle should accidently fall, ");
+		listTest.add("They'll be ");
+		listTest.add(" green bottle hanging on the wall");
+		listTest.add("no bottles of beer on the wall");
 
 
 
-		" green bottles of beer on the wall", " and if ",
-				" green bottle should accidently fall, ", "They'll be ", " green bottle hanging on the wall",
-				"no bottles of beer on the wall"
+
+		
+			
 
 		System.out.println("is it working?");
 		
 		System.out.println(listTest.get(0));
+		System.out.println(listTest.get(1));
+		System.out.println(listTest.get(2));
+		System.out.println(listTest.get(3));
+		System.out.println(listTest.get(4));
+		System.out.println(listTest.get(5));
+		
+		System.out.println("\n part 2");
+		
+		//enhanced for loop to go through the arraylist
+		
+		for (String b : listTest){
+			
+			System.out.println(b);
+			
+			
+		}
+		
+		
 		
 		
 	}
