eb6fe7902839c8c5d9393a2a685e4daf23748394
shane <shanecunningham@live.ie>
2016-10-20 15:35:49 +0100

    add interface

--- WeekTwo/src/com/ThirtyFour/Warrior.java
+++ WeekTwo/src/com/ThirtyFour/Warrior.java
@@ -6,7 +6,7 @@ public class Warrior implements behaviour {
 	public void happy() {
 		// TODO Auto-generated method stub
 
-		System.out.println("i'm happy warrior");
+		System.out.println("i'm happy warrior warrior");
 
 	}
 
@@ -37,6 +37,64 @@ public class Warrior implements behaviour {
 
 		System.out.println("my happiest is:" + num + "my name is:" + name);
 
+	}
+
+}
+
+class Wizard implements behaviour, Movement {
+
+	@Override
+	public void walk() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void swim() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void happy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void angry() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scared() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void frusated(double num, String name) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
