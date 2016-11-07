a2efa917775dbf6e25cf8a38cec9d2d62d6a0d5c
shane <shanecunningham@live.ie>
2016-11-07 14:39:34 +0000

    explain of when garbage collection kicks off

--- WeekFive/SectionThreeTwo/src/com/android/Main.java
+++ WeekFive/SectionThreeTwo/src/com/android/Main.java
@@ -18,22 +18,32 @@ public class Main {
         /*
         * declares a variable of type person , it does not  create a person
         * */
-        Person bob;
+        //  Person bob;
 
         // an object is  only created when a reference var is initialised
 
-        Person fred = new Person();
+        //  Person fred = new Person();
 
-        Person paul = new Person();
+        //   Person paul = new Person();
 
         //  Person john = new Person();
-        fred = paul;
-         Person patsy;
-        patsy = paul;
+        //  fred = paul;
+        //    Person patsy;
+        //   patsy = paul;
 
         // you can't reference this, it has no name , its still created and is liable for garage collection
 
 
+        Person paul = new Person();
+
+        Person peter = new Person();
+
+        Person patsy = peter = paul;
+
+        peter = new Person();
+
+        patsy = peter;
+        paul = new Person();
 
     }
 }
