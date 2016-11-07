db83ceacca5dc882e81b3bd023d6a08655afec27
shane <shanecunningham@live.ie>
2016-11-07 13:38:45 +0000

    start of scala test

---
+++ WeekFour/ScalaTest/src/HelloWorld.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by shane on 04/11/2016.
+  */
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello, world!")
+  }
+}
---
+++ WeekFour/ScalaTest/src/main.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by shane on 03/11/2016.
+  */
+class main {
+
+  object HelloWorld {
+    def main(args: Array[String]) {
+      println("Hello, world!")
+    }
+  }
+
+}
