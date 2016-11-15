7ccb7c3c9c53b7050bf7762f923cb871280f0570
shane <shanecunningham@live.ie>
2016-11-15 10:23:43 +0000

    3D arrays

---
+++ WeekSix/Chapter4/src/com/section/section/three/Main.java
@@ -0,0 +1,17 @@
+package com.section.section.three;
+
+/**
+ * Created by shane on 15/11/2016.
+ */
+public class Main {
+
+
+
+    public static void main(String args []){
+
+
+        System.out.println("test out");
+    }
+}
+
+
