076e4ef9fe9a6d99d047aa8bcb222563757acf33
shane <shanecunningham@live.ie>
2016-11-15 09:10:35 +0000

    start of week six

---
+++ WeekSix/Testing/src/com/shane/cunningham/GuessingNumberGame.java
@@ -0,0 +1,16 @@
+package com.shane.cunningham;
+
+/**
+ * Created by shane on 15/11/2016.
+ */
+public class GuessingNumberGame {
+
+
+    public static void guessing() {
+
+
+
+        
+
+    }
+}
---
+++ WeekSix/Testing/src/com/shane/cunningham/Main.java
@@ -0,0 +1,13 @@
+package com.shane.cunningham;
+
+/**
+ * Created by shane on 15/11/2016.
+ */
+public class Main {
+
+    public static void main (String args []){
+
+
+
+    }
+}
