5bdbd3e4c51012881684546db1484ea4edb6516a
shane <shanecunningham@live.ie>
2016-11-16 09:01:59 +0000

    finished guessing game,

--- WeekSix/Chapter4/src/com/section/section/three/Main.java
+++ WeekSix/Chapter4/src/com/section/section/three/Main.java
@@ -28,7 +28,11 @@ public class Main {
         //Examples.ex6();
 
         //Examples.ex7();
-        Examples.ex8();
+        //  Examples.ex8();
+
+        //  Examples.ex9();
+
+        Examples.ex10();
     }
 
 
--- WeekSix/Testing/src/com/shane/cunningham/GuessingNumberGame.java
+++ WeekSix/Testing/src/com/shane/cunningham/GuessingNumberGame.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 /**
  * Created by shane on 15/11/2016.
  */
-public class GuessingNumberGame {
+class GuessingNumberGame {
 
 
-    public static void guessing() {
-
-        System.out.println("Random number guessing game ");
+    static void guessing() {
+        int tries = 7;
+        System.out.println("Random number guessing game\nYou have " + tries + " attempts to guess the secret number");
 
         Scanner keyboard = new Scanner(System.in);
         Random rand = new Random();
@@ -26,31 +26,34 @@ public class GuessingNumberGame {
         System.out.println("random number is : " + randNum);
         System.out.println("your guess is " + guess);
         boolean test = false;
-        while (!test) {
-            if (randNum > guess) {
-                System.out.println("your number is lower");
-                System.out.println("Please try again: ");
-                guess = keyboard.nextInt();
+
+
+        while (!(test)) {
+            tries--;
+
+            System.out.println("attempts left: " + tries);
+            if (tries <= 0) {
+                System.out.println("You lost, too many guesses\n" + "The secret number was: " + randNum);
+                test = true;
             } else if (randNum < guess) {
 
                 System.out.println("your number is higher");
                 System.out.println("Please try again: ");
 
                 guess = keyboard.nextInt();
+            } else if (randNum > guess) {
+                System.out.println("your number is lower");
+                System.out.println("Please try again: ");
+                guess = keyboard.nextInt();
             } else {
 
-                System.out.println("you won");
+                System.out.println("Congratulations you won");
                 test = true;
-    }
+            }
+
         }
+
     }
 
 
-    int add;
-    public int add(int add){
-
-        this.add = add;
-
-        return add;
-    }
 }
