13a9e1732817ad77974ab40a1e4c739eb93932ee
shane <shanecunningham@live.ie>
2016-11-18 10:10:22 +0000

    added comments

--- WeekSix/Testing/src/com/shane/cunningham/Main.java
+++ WeekSix/Testing/src/com/shane/cunningham/Main.java
@@ -12,19 +12,19 @@ public class Main {
 
         //GuessingNumberGame.guessing();
 
-        Scanner keyboard = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in); // creates a new scanner object called keyboard to take input from the keyboard
 
         System.out.println("Enter size of star pattern, make sure it a odd number:");
 
 
-        int size = keyboard.nextInt();
+        int size = keyboard.nextInt(); // take in a int from the keyboard
 
-        if (size % 2 == 0) {
+        if (size % 2 == 0) { // check to see if it is a even number
 
             System.out.println("you entered a even number \nPlease enter a odd number: ");
-            size = keyboard.nextInt();
+            size = keyboard.nextInt(); // reset the int size variable
         }
-        Star.starTest(size);
+        Star.starTest(size); // calls the startest method and pass the size variable to it
 
     }
 }
--- WeekSix/Testing/src/com/shane/cunningham/Star.java
+++ WeekSix/Testing/src/com/shane/cunningham/Star.java
@@ -1,7 +1,5 @@
 package com.shane.cunningham;
 
-import java.util.Scanner;
-
 /**
  * Created by shane on 17/11/2016.
  */
@@ -12,20 +10,21 @@ public class Star {
 
 
         int i, j, k;
-        for (i = 1; i <= size; i++) {
-            for (j = i; j < size; j++) {
+        for (i = 1; i <= size; i++) { // outer loop through until it is less than or equal to the size variable
+            for (j = i; j < size; j++) {// use the loop to add a spaces
                 System.out.print(" ");
             }
-            for (k = 1; k < (i * 2); k++) {
+            for (k = 1; k < (i * 2); k++) {// uses the loop to add *  multi by 2
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println();// print a new line at the end of the loop
         }
-        for (i = (size - 1); i >= 1; i--) {
+        for (i = (size - 1); i >= 1; i--) {// outer loop through until it is greater than or equal to the size variable,
+            // keep decreasing  j
             for (j = size; j > i; j--) {
                 System.out.print(" ");
             }
-            for (k = 1; k < (i * 2); k++) {
+            for (k = 1; k < (i * 2); k++) {// uses the loop to add *  multi by 2
                 System.out.print("*");
             }
             System.out.println();
