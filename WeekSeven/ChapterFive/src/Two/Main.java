05205b61b75e03f50f4b031dfadcf39f9500e51c
shane <shanecunningham@live.ie>
2016-11-24 10:34:12 +0000

    if statements

---
+++ WeekSeven/ChapterFive/src/Two/Main.java
@@ -0,0 +1,12 @@
+package Two;
+
+/**
+ * Created by shane on 24/11/2016.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+
+
+    }
+}
--- WeekSeven/ChapterFive/src/one/Main.java
+++ WeekSeven/ChapterFive/src/one/Main.java
@@ -4,9 +4,16 @@ package one;
  * Created by shane on 23/11/2016.
  */
 public class Main {
-    
+
 
     public static void main(String args[]) {
+        int score2 = 455;
+
+        // Examples.ex1();
+        // Examples.ex2();
+        // Examples.ex3();
+
+        Examples.ex4(score2);
 
 
     }
