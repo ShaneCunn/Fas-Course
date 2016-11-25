7cfb902894c78af015a1db1df34fa3f1189f8b38
shane <shanecunningham@live.ie>
2016-11-25 11:02:59 +0000

    enhanced for loop ie for each

--- WeekSeven/ChapterFive/src/com/four/Examples.java
+++ WeekSeven/ChapterFive/src/com/four/Examples.java
@@ -1,7 +1,63 @@
 package com.four;
 
+import java.util.ArrayList;
+
 /**
  * Created by shane on 25/11/2016.
  */
 public class Examples {
+
+
+    static void ex1() {
+
+
+        ArrayList<String> strList = new ArrayList<String>();
+
+
+        strList.add("mary");
+        strList.add("patsy");
+        strList.add("harry");
+        strList.add("bridie");
+        //   strList.add("mary");
+
+
+        for (String p : strList) {
+
+
+            System.out.println(p);
+        }
+        String[] nameList = {"mary", "patsy", "harry", "bridie"};
+
+
+        for (String p : nameList) {
+
+            System.out.println(p);
+        }
+
+
+        int[] numArray = {34, 56, 67, 78, 89, 99};
+
+        for (int num : numArray) {
+            System.out.println(num);
+
+        }
+
+        StringBuilder[] sbArray = {new StringBuilder("timm"), new StringBuilder("george"), new StringBuilder("sarah")};
+
+        for (StringBuilder sb : sbArray) {
+
+
+            sb = new StringBuilder("harry");
+
+            System.out.println(sb);
+        }
+
+        for (StringBuilder sb : sbArray) {
+
+
+            System.out.println(sb);
+        }
+
+
+    }
 }
---
+++ WeekSeven/ChapterFive/src/com/four/Main.java
@@ -0,0 +1,13 @@
+package com.four;
+
+/**
+ * Created by shane on 25/11/2016.
+ */
+public class Main {
+
+
+    public static void main(String args[]) {
+
+        Examples.ex1();
+    }
+}
