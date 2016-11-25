69e512043845eba928f29fa27ae8d15ea06e626f
shane <shanecunningham@live.ie>
2016-11-25 11:43:13 +0000

    while and do while loops

---
+++ WeekSeven/ChapterFive/src/com/five/Examples.java
@@ -0,0 +1,19 @@
+package com.five;
+
+/**
+ * Created by shane on 25/11/2016.
+ */
+public class Examples {
+
+
+    public static void ex1() {
+        int age = 12;
+
+        while (age < 18) {
+
+            System.out.println("your age is " + age);
+            age++;
+        }
+
+    }
+}
---
+++ WeekSeven/ChapterFive/src/com/five/Main.java
@@ -0,0 +1,14 @@
+package com.five;
+
+/**
+ * Created by shane on 25/11/2016.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+
+        //  exam objective 5.1  create and use while and do while loops
+
+        Examples.ex1();
+    }
+}
