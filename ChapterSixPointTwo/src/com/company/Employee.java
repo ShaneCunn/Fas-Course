916e97dc159a6a24920d9f5e99ded6524bcc547d
Shane Cunningham <shanecunningham@live.ie>
2016-11-29 11:48:09 +0000

    chapter six start

---
+++ ChapterSixPointTwo/src/com/company/Employee.java
@@ -0,0 +1,32 @@
+package com.company;
+
+/**
+ * Created by shane on 29/11/2016.
+ */
+public class Employee extends Person {
+    double wage = 90000;
+    int hoursWorked = 25;
+
+    void eat() {
+
+        System.out.println("Employee eating");
+
+
+    }
+
+
+    void takesBreaks() {
+
+        System.out.println("Employee taking break");
+
+
+    }
+
+    void calcTax() {
+
+        System.out.println("Employee taxes are");
+
+
+    }
+
+}
---
+++ ChapterSixPointTwo/src/com/company/Person.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ * Created by shane on 29/11/2016.
+ */
+public class Person {
+
+
+    String name = "bob";
+    int age = 20;
+    double weight = 10.5;
+    double hieght = 2.3;
+
+    void eat() {
+
+        System.out.println("person eating");
+    }
+
+    void drink() {
+
+        System.out.println("person drinking");
+    }
+}
