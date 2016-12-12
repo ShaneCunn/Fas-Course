eab033033b2436bc3c9bf54ef61315215e8939f9
Shane Cunningham <shanecunningham@live.ie>
2016-12-12 14:37:24 +0000

    start of six point 2, interfaces and abstract classes

---
+++ ChapterSixOne/Chapter six point two/src/com/android/Examples.java
@@ -0,0 +1,12 @@
+package com.android;
+
+/**
+ * Created by shane on 12/12/2016.
+ */
+public class Examples {
+
+
+
+
+
+}
---
+++ ChapterSixOne/Chapter six point two/src/com/android/Main.java
@@ -0,0 +1,17 @@
+package com.android;
+
+/**
+ * Created by shane on 12/12/2016.
+ * <p>
+ * <p>
+ * exam objective  7.1  describe inheritance and it benefits
+ * <p>
+ * exam objective 7.5  use abstract classes and interfaces
+ */
+public class Main {
+
+    public static void main(String args[]) {
+
+
+    }
+}
