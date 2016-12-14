f8bcb615e5f5fb60c7987b897b7ae7859901255a
Shane Cunningham <shanecunningham@live.ie>
2016-12-14 12:16:55 +0000

    start of polymorphism and reference types

---
+++ ChapterSixOne/six.Three/src/com/android/Employee.java
@@ -0,0 +1,14 @@
+package com.android;
+
+/**
+ * Created by shane on 14/12/2016.
+ * <p>
+ * <p>
+ * Develop code that shows the use of polymorphism
+ * including overridding and object ype versus reference type
+ */
+public class Employee {
+
+    int age;
+    String name;
+}
