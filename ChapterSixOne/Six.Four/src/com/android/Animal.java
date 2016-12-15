7766fcbacf2c491526ac99d06adc499a81d29a19
Shane Cunningham <shanecunningham@live.ie>
2016-12-15 10:28:29 +0000

    casting

---
+++ ChapterSixOne/Six.Four/src/com/android/Animal.java
@@ -0,0 +1,60 @@
+package com.android;
+
+/**
+ * Created by shane on 14/12/2016.
+ */
+public class Animal {
+
+    int age = 4;
+    String name = "andy";
+
+    void eat() {
+
+        System.out.println("animal eating");
+    }
+
+}
+
+class Cow extends Animal {
+    @Override
+    void eat() {
+        //  super.eat();
+    }
+
+    void Drink() {
+        System.out.println("Cow drinking");
+    }
+}
+
+class Sheep extends Animal {
+    void sleep() {
+
+        System.out.println("Sheep sleeping");
+    }
+
+}
+
+class Pig extends Animal {
+
+    void pigging() {
+
+        System.out.println("pig pigging");
+    }
+}
+
+class Chicken extends Animal {
+
+    void chucking() {
+
+        System.out.println("chicken make noises");
+    }
+
+
+}
+
+class hen extends Chicken {
+
+    void feeding() {
+        System.out.println("hen feeding");
+    }
+}
--- ChapterSixOne/Six.Four/src/com/android/Main.java
+++ ChapterSixOne/Six.Four/src/com/android/Main.java
@@ -16,7 +16,61 @@ public class Main {
         long lnum = (long) dnum;
 
         System.out.println(lnum);
+        Cat tibbles = new Cat();
 
+
+        // Dog spot = new Cat();
+
+        // Dog spot = tibbles;
+        //  Dog spot = (Dog)tibbles;
+
+
+        Animal bob = new Animal();
+        bob.eat();
+        System.out.println("age: " + bob.age + " Name is: " + bob.name);
+
+        // you can cast it a lower class ie derived class, it can do this because it
+        //Cow bessie = (Cow) bob;
+        hen chicks = new hen();
+        Animal[] testArray = {
+
+                bob = new Chicken(),
+                new Cow(),
+                new Sheep(),
+                new Pig(),
+                chicks = new hen()
+
+        };
+
+        System.out.println(testArray[0] instanceof Cow);
+        System.out.println(testArray[0] instanceof Chicken);
+        int numCount = 0;
+
+        for (Animal b : testArray) {
+            if (b instanceof Chicken) {
+
+                System.out.println("True");
+                System.out.println(Chicken.class.getSuperclass());
+            } else if (b instanceof Cow) {
+                numCount++;
+
+
+            } else if (b instanceof Pig) {
+                numCount++;
+
+
+            } else if (b instanceof Sheep) {
+                numCount++;
+
+
+            } else if (b instanceof hen) {
+                numCount++;
+
+
+            }
+
+        }
+        System.out.println("Other Animals: " + numCount);
 
     }
 }
